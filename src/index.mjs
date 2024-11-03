f2e294ad6b50a0468011a232c1b096bd4dd0ed34
Andrei <dandrei.web@gmail.com>
2024-11-03 14:11:11 +0100

    Update: added polling every 5 min

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,24 @@
-import { fetchLatestPost } from './services/wordpress.mjs';
-import { sendPostToTelegram } from './services/telegram.mjs';
+import { fetchLatestPost } from "./services/wordpress.mjs";
+import { sendPostToTelegram } from "./services/telegram.mjs";
 
 async function main() {
   try {
     const latestPost = await fetchLatestPost();
     if (latestPost) {
       await sendPostToTelegram(latestPost);
-      console.log('Post successfully sent to Telegram!');
+      console.log("Post successfully sent to Telegram!");
     }
   } catch (error) {
-    console.error('Error in main process:', error);
+    console.error("Error in main process:", error);
   }
 }
 
 main();
+
+const POLLING_INTERVAL = 1000 * 60 * 5;
+setInterval(main, POLLING_INTERVAL); // Run every 5 minutes
+
+process.on("SIGINT", () => {
+  console.log("Bot shutting down..");
+  process.exit(0);
+});
--- src/services/wordpress.mjs
+++ src/services/wordpress.mjs
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { config } from "../config/config.mjs";
 
+let lastProcessedPostId = null;
+
 export async function fetchLatestPost() {
   try {
     const response = await axios.get(
@@ -12,7 +14,7 @@ export async function fetchLatestPost() {
       }
     );
 
-    return {
+    const post = {
       id: response.data[0].id,
       date: response.data[0].date,
       link: response.data[0].link,
@@ -22,6 +24,14 @@ export async function fetchLatestPost() {
         response.data[0]._embedded?.["wp:featuredmedia"]?.[0]?.source_url ||
         null,
     };
+
+    // If no post ID has been set yet, or the current post ID is greater than the last processed post ID, return the post`
+    if (lastProcessedPostId === null || post.id > lastProcessedPostId) {
+      lastProcessedPostId = post.id;
+      return post;
+    }
+
+    return null; // No new post found
   } catch (err) {
     console.error("WordPress API Error:", err);
     throw new Error(err.message);
