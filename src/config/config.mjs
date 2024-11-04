c56fb7561e8b8aea7a73027b36afa6caab7844c2
Andrei <dandrei.web@gmail.com>
2024-11-04 18:57:53 +0100

    env error handle

--- src/config/config.mjs
+++ src/config/config.mjs
@@ -1,5 +1,13 @@
 import dotenv from "dotenv";
+import assert from "node:assert";
+
 dotenv.config();
+
+assert(process.env.TELEGRAM_TOKEN, "TELEGRAM_TOKEN is required");
+assert(process.env.TELEGRAM_CHANNEL_ID, "TELEGRAM_CHANNEL_ID is required");
+assert(process.env.WP_USERNAME, "WP_USERNAME is required");
+assert(process.env.WP_APP_PWD, "WP_APP_PWD is required");
+assert(process.env.WP_DOMAIN, "WP_DOMAIN is required");
 
 export const config = {
   telegram: {
